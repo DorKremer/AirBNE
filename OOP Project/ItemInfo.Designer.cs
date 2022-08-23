87ccc4b306aa34478ff1c1fd33ab9860e03fe2cf
Michael Denis <Michael.Denis@s.afeka.ac.il>
2022-08-23 19:03:10 +0300

    sfsdf

--- OOP Project/ItemInfo.Designer.cs
+++ OOP Project/ItemInfo.Designer.cs
@@ -31,41 +31,64 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ItemInfo));
             this.label1 = new System.Windows.Forms.Label();
             this.orderBox = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(209, 34);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(125, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(59, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
             // orderBox
             // 
-            this.orderBox.Location = new System.Drawing.Point(66, 319);
+            this.orderBox.Location = new System.Drawing.Point(88, 393);
+            this.orderBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.orderBox.Name = "orderBox";
-            this.orderBox.Size = new System.Drawing.Size(339, 80);
+            this.orderBox.Size = new System.Drawing.Size(452, 98);
             this.orderBox.TabIndex = 1;
             this.orderBox.Text = "Order";
             this.orderBox.UseVisualStyleBackColor = true;
             this.orderBox.Click += new System.EventHandler(this.orderBox_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.groupBox1.Location = new System.Drawing.Point(132, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(366, 357);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CheckIn Information";
+            // 
             // ItemInfo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(469, 411);
+            this.BackgroundImage = global::OOP_Project.Properties.Resources.BETransparentBlackLogocopy;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(625, 506);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.orderBox);
-            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ItemInfo";
             this.Text = "ItemInfo";
             this.Load += new System.EventHandler(this.ItemInfo_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -73,5 +96,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button orderBox;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
